f7482a0c5a343c7a5bb4aa48fd37f5f3d0b9005e
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-09-02 01:45:35 -0500

    Add Axios and API call to Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
 import logo from './logo.png';
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+        <div className="container">
       <header className="App-header">
         <img src={logo}
              className="App-logo img-fluid"
              alt="logo" />
       </header>
+
+            <main>
+                <Dictionary />
+            </main>
+
+            <footer className="App-footer">
+                <small>
+                    Coded by Julicia
+                </small>
+            </footer>
+        </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
+    function search(event) {
+        event.preventDefault();
+
+        let apiKey = "otb198570afbd1823c32f524f4467bab";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleKeywordChange(event) {
+        // stores the inputted value in the state
+        setKeyword(event.target.value);
+    }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange}/>
+            </form>
+        </div>
+    );
+}
