daa120f1474c98f58bef20a665e2d89040fad862
codeceaux <jcarrollinc@gmail.com>
2024-09-02 16:12:56 -0500

    Refactor Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Synonyms from './Synonyms';
+
 
 export default function Meaning(props) {
     console.log(props.meaning);
-        return (
-            <div>
-                <h3>{props.meaning.partOfSpeech}</h3>
-                <p>{props.meaning.definition}</p>
-                <p>{props.meaning.example}</p>
-            </div>
-        );
+    return (
+        <div className="Meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>
+            <p>
+                <strong>Definition:</strong> {props.meaning.definition}
+                <br />
+                <strong>Example:</strong>
+                <em> {props.meaning.example}</em>
+                <br />
+            </p>
+            <Synonyms synonyms={props.meaning.synonyms} />
+        </div>
+    );
 }
