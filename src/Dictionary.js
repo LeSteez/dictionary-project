6a962e80dade2093b42495ce570bfd5ea72cfefa
codeceaux <jcarrollinc@gmail.com>
2024-09-02 17:17:38 -0500

    Remove loaded function from Dictionary,js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,24 +6,20 @@ import "./Dictionary.css";
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
-    let [loaded,  setLoaded] = useState(false);
 
     function handleResponse(response) {
         console.log(response.data);
         setResults(response.data);
     }
 
-    function search() {
+    function search(event) {
+        event.preventDefault();
+
         // documentation: https://www.shecodes.io/learn/apis/dictionary
         let apiKey = "otb198570afbd1823c32f524f4467bab";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-        // api call
-        axios.get(apiUrl).then(handleResponse);
-    }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        search();
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
@@ -31,27 +27,17 @@ export default function Dictionary() {
         setKeyword(event.target.value);
     }
 
-    function load() {
-        setLoaded(true);
-        search();
-    }
-}
-
-    if (loaded) {
-        return (
-            <div className="Dictionary">
-                <section>
-                    <form onSubmit={handleSubmit}>
-                        <input type="search" onChange={handleKeywordChange}/>
-                    </form>
-                    <div className="hint">
-                        suggested words: sunset, wine, mundane...
-                    </div>
-                </section>
+    return (
+        <div className="Dictionary">
+            <section>
+                <form onSubmit={search}>
+                    <input type="search" onChange={handleKeywordChange}/>
+                </form>
+                <div className="hint">
+                    suggested words: sunset, wine, mundane...
+                </div>
+            </section>
                 <Results results={results} />
-            </div>
-        );
-    } else {
-        load();
-        return null;
+        </div>
+    );
 }
