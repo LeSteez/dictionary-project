8f20bb63762529923a6b17b4fa58eda72e0c6f14
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-09-02 01:48:02 -0500

    Add API documentation link comment

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,7 @@ export default function Dictionary() {
     function search(event) {
         event.preventDefault();
 
+        // documentation: https://www.shecodes.io/learn/apis/dictionary
         let apiKey = "otb198570afbd1823c32f524f4467bab";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
